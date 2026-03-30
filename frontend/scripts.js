830210493bfc220b14897d71e96bfb0a00b6f64b
Sparrow535 <rohanrai4133@gmail.com>
2026-03-30 15:18:15 +0600

    ui changes

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -379,7 +379,7 @@
       teaserTitle.textContent = "Kuzuzangpo la! How may I assist you today?";
 
       if (teaserSubtitle) {
-        teaserSubtitle.innerHTML = '<span class="teaser-online">Online</span>';
+        teaserSubtitle.textContent = "";
       }
     }
 
